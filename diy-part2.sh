8ba9f940008ea438db5ac3d2aea1462e8cd1438e
lphgor <3890346+lphgor@users.noreply.github.com>
2020-07-11 05:19:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,9 @@
 #============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+
+sed -i 's/^GO_VERSION_PATCH:=$/GO_VERSION_PATCH:=4/g;s/6d643e46ad565058c7a39dac01144172ef9bd476521f42148be59249e4b74389/7011af3bbc2ac108d1b82ea8abb87b2e63f78844f0259be20cde4d42c5c40584/g' ./feeds/packages/lang/golang/golang/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
+sed -i '/nas/d;/admin_status/d;/99999/d;/openwrt_release/d;/log-facility/d' ./package/lean/default-settings/files/zzz-default-settings
+echo 0xDEADBEEF > ./feeds/helloworld/luci-app-ssr-plus/root/etc/config/google_fu_mode
