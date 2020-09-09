932af424cc56ec44889fb9af84ac3c8ea295147c
lphgor <3890346+lphgor@users.noreply.github.com>
2020-09-09 08:52:17 +0800

    Update k2p.diy-part1.sh

--- k2p.diy-part1.sh
+++ k2p.diy-part1.sh
@@ -17,4 +17,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 rm -rf ./package/lean/UnblockNeteaseMusic*
 rm -rf ./package/lean/luci-app-unblockmusic
 git clone https://github.com/cnsilvan/luci-app-unblockneteasemusic ./package/unblockneteasemusic
+sed -i 's/aa1eb0186206671de9a6b4fa27d933bb306d4e25/1c6951389ed2b4ed3b5a35052ca35a9647ae74d001b0af07180a66aa0f77e816/g' ./package/unblockneteasemusic/UnblockNeteaseMusic/Makefile
 git clone https://lphgor:$1@github.com/lphgor/mosquitto.git ./package/mosquitto
