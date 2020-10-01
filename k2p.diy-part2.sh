f000295b1f9a501cc3a20eeb5b11dca4ece124f0
lphgor <3890346+lphgor@users.noreply.github.com>
2020-10-01 19:32:12 +0800

    Update k2p.diy-part2.sh

--- k2p.diy-part2.sh
+++ k2p.diy-part2.sh
@@ -15,6 +15,6 @@ sed -i 's/^GO_VERSION_MAJOR_MINOR:=1.14$/GO_VERSION_MAJOR_MINOR:=1.14/g;s/^GO_VE
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
 sed -i 's/20200701/20200930/g;s/d5b2d212c6806f1c4eba5fbce8797734eaa8ae0f8cdd90dd06d0844392888ff0/90d0851e96ff723d4bcd7fd00795e926418ce5cf17c841e1e0185b0770b84874/g' ./package/lean/kcptun/Makefile
 sed -i 's/GO_PKG_LDFLAGS:=/GO_PKG_MOD:=vendor\nGO_PKG_LDFLAGS:=/' ./package/lean/kcptun/Makefile
-sed -i 's/$(if $(GO_PKG_GCFLAGS),-gcflags/$(if $(GO_PKG_MOD),-mod="$(GO_PKG_MOD)") \\\n\t$(if $(GO_PKG_GCFLAGS),-gcflags/' ./feeds/packages/lang/golang/golang-package.mk
+sed -i 's/$(if $(GO_PKG_GCFLAGS),-gcflags/$(if $(GO_PKG_MOD),-mod=$(GO_PKG_MOD)) \\\n\t$(if $(GO_PKG_GCFLAGS),-gcflags/' ./feeds/packages/lang/golang/golang-package.mk
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF/$1$l1CYLqFu$llSM4AY38sLaTb7gelfl0/g;/nas/d;/admin_status/d;/ustclug/d;/openwrt_release/d;/log-facility/d' ./package/lean/default-settings/files/zzz-default-settings
 echo 0xDEADBEEF > ./feeds/helloworld/luci-app-ssr-plus/root/etc/config/google_fu_mode
