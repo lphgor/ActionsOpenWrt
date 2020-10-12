6eb9ec8302944d06db44cee61a5ab6d9f33f4c1e
lphgor <3890346+lphgor@users.noreply.github.com>
2020-10-12 16:59:01 +0800

    Update k2p.diy-part2.sh

--- k2p.diy-part2.sh
+++ k2p.diy-part2.sh
@@ -13,7 +13,7 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 
 sed -i 's/^GO_VERSION_MAJOR_MINOR:=1.14$/GO_VERSION_MAJOR_MINOR:=1.14/g;s/^GO_VERSION_PATCH:=4$/GO_VERSION_PATCH:=9/g;s/7011af3bbc2ac108d1b82ea8abb87b2e63f78844f0259be20cde4d42c5c40584/c687c848cc09bcabf2b5e534c3fc4259abebbfc9014dd05a1a2dc6106f404554/g' ./feeds/packages/lang/golang/golang/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
-sed -i 's/20200701/20201004/g;s/d5b2d212c6806f1c4eba5fbce8797734eaa8ae0f8cdd90dd06d0844392888ff0/35d33bda5b643f07fab8b211cc1181298e27781e558bfc94b77491ea6abe61af/g' ./package/lean/kcptun/Makefile
+sed -i 's/20200701/20201010/g;s/d5b2d212c6806f1c4eba5fbce8797734eaa8ae0f8cdd90dd06d0844392888ff0/afab2a087b787e59e129c7d4fbc578e1131d264c0da1ce23cd1282321fc3c189/g' ./package/lean/kcptun/Makefile
 # sed -i 's#codeload.github.com/xtaci#codeload.github.com/lphgor#g' ./package/lean/kcptun/Makefile
 # sed -i 's/GO_PKG_LDFLAGS:=/GO_PKG_MOD:=vendor\nGO_PKG_LDFLAGS:=/' ./package/lean/kcptun/Makefile
 # sed -i 's/$(if $(GO_PKG_GCFLAGS),-gcflags/$(if $(GO_PKG_MOD),-mod=$(GO_PKG_MOD)) \\\n\t$(if $(GO_PKG_GCFLAGS),-gcflags/' ./feeds/packages/lang/golang/golang-package.mk
