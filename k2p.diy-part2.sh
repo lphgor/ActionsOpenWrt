87d883b43a45c3f77b99daf862b4cda91211bca8
lphgor <3890346+lphgor@users.noreply.github.com>
2021-04-26 11:56:16 +0800

    Update k2p.diy-part2.sh

--- k2p.diy-part2.sh
+++ k2p.diy-part2.sh
@@ -18,4 +18,6 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF/$1$l1CYLqFu$llSM4AY38sLaTb7gelfl0/g;
 #sed -i "s#exit 0#sed -i '/helloworld/d' /etc/opkg/distfeeds.conf\nexit 0#g" ./package/lean/default-settings/files/zzz-default-settings
 echo 0xDEADBEEF > ./feeds/helloworld/luci-app-ssr-plus/root/etc/config/google_fu_mode
 
+# Remove ssr & simple-obfs
+sed -i 's/+shadowsocksr-libev-alt //g;s/+shadowsocksr-libev-ssr-local //g;/simple-obfs/d' feeds/helloworld/luci-app-ssr-plus/Makefile
 #sed -i 's/> 60/> 59/g;s/> 24/> 23/g' ./feeds/luci/modules/luci-base/htdocs/luci-static/resources/cbi.js
