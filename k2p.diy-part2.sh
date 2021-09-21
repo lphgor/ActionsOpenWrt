d718ac20042229e6a613ae7d505b22ef3c58b7d5
lphgor <3890346+lphgor@users.noreply.github.com>
2021-09-21 20:59:49 +0800

    Update k2p.diy-part2.sh

--- k2p.diy-part2.sh
+++ k2p.diy-part2.sh
@@ -13,6 +13,7 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF/$1$l1CYLqFu$llSM4AY38sLaTb7gelfl0/g;/nas/d;/admin_status/d;/ustclug/d;/openwrt_release/d;/log-facility/d' ./package/lean/default-settings/files/zzz-default-settings
+sed -i '/wgetrc/ised -i "/DISTRIB_DESCRIPTION/ { s/SNAPSHOT/SNAPSHOT($(date +%Y.%m.%d))/g; }" /etc/openwrt_release' ./package/lean/default-settings/files/zzz-default-settings
 echo 0xDEADBEEF > ./feeds/helloworld/luci-app-ssr-plus/root/etc/config/google_fu_mode
 sed -i '/DEPENDS:=+kcptun-config/d' ./feeds/packages/net/kcptun/Makefile
 sed -i 's/spi-max-frequency = <80000000>/spi-max-frequency = <10000000>/g' ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
