60873867129eecbaf1815bbb2f9baa71cad29158
lphgor <3890346+lphgor@users.noreply.github.com>
2022-09-30 14:34:53 +0800

    Update k2p.diy-part2.sh

--- k2p.diy-part2.sh
+++ k2p.diy-part2.sh
@@ -19,4 +19,4 @@ echo 0xDEADBEEF > ./feeds/helloworld/luci-app-ssr-plus/root/etc/config/google_fu
 sed -i '/DEPENDS:=+kcptun-config/d;s/20210922/20220628/g;s/2021-09-22/2022-06-28/g;s/f6a08f0fe75fa85d15f9c0c28182c69a5ad909229b4c230a8cbe38f91ba2d038/6a63facc902594b4ca5f0456e58196cf7b2a2451594fe2f69b55ac712ceb85e8/g;' ./feeds/packages/net/kcptun/Makefile
 sed -i 's/spi-max-frequency = <80000000>/spi-max-frequency = <10000000>/g' ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i '/list listen_https/d' ./package/network/services/uhttpd/files/uhttpd.config
-sed -i 's/9625784cf2e4fd9842f1d407681ce4878b5b0dcddbcd31c6135114a30c71e6a8/5de8c8e29aaa3fb9cc6b47bb27299f271354ebb72514e3accadc7d38b5bbaa72/g' ./feeds/packages/utils/jq/Makefile
+# sed -i 's/9625784cf2e4fd9842f1d407681ce4878b5b0dcddbcd31c6135114a30c71e6a8/5de8c8e29aaa3fb9cc6b47bb27299f271354ebb72514e3accadc7d38b5bbaa72/g' ./feeds/packages/utils/jq/Makefile
