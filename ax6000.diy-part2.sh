5566c0cb47860c9b8f51c13471e5550bd4630780
lphgor <3890346+lphgor@users.noreply.github.com>
2024-02-28 18:45:17 +0800

    Update ax6000.diy-part2.sh

--- ax6000.diy-part2.sh
+++ ax6000.diy-part2.sh
@@ -15,4 +15,4 @@ sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/l
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci-ssl-nginx/Makefile
 sed -i '/list listen_https/d' ./package/network/services/uhttpd/files/uhttpd.config
 sed -i '/log-facility/d' ./package/emortal/default-settings/files/99-default-settings
-sed -i "/exit 0/ised -i '/DISTRIB_DESCRIPTION/ { s/SNAPSHOT/SNAPSHOT($(date +%Y.%m.%d-%H%M%S))/g; }' /etc/openwrt_release" ./package/emortal/default-settings/files/99-default-settings
+sed -i "/exit 0/ised -i '/DISTRIB_DESCRIPTION/ { s/SNAPSHOT/SNAPSHOT($(date +%Y%m%d%H%M%S))/g; }' /etc/openwrt_release" ./package/emortal/default-settings/files/99-default-settings
