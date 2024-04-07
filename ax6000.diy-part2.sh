c6a9e818ffdd8ef3c0c621609da1ae7cf67c9694
lphgor <3890346+lphgor@users.noreply.github.com>
2024-04-07 20:03:36 +0800

    Update ax6000.diy-part2.sh

--- ax6000.diy-part2.sh
+++ ax6000.diy-part2.sh
@@ -10,9 +10,9 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' ./package/base-files/files/bin/config_generate
-#sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci-light/Makefile
-#sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci-nginx/Makefile
-#sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci-ssl-nginx/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci-light/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci-nginx/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci-ssl-nginx/Makefile
 sed -i '/list listen_https/d' ./package/network/services/uhttpd/files/uhttpd.config
 sed -i '/log-facility/d' ./package/emortal/default-settings/files/99-default-settings
 sed -i "/exit 0/ised -i '/DISTRIB_DESCRIPTION/ { s/SNAPSHOT/SNAPSHOT($(date +%Y%m%d%H%M%S))/g; }' /etc/openwrt_release" ./package/emortal/default-settings/files/99-default-settings
