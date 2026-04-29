2bd6c655c6c9d3ab8fe5e7dc969932a83ad26685
lphgor <3890346+lphgor@users.noreply.github.com>
2026-04-30 04:23:50 +0800

    Uncomment sed command to update Makefile

--- ax6000.diy-part1.sh
+++ ax6000.diy-part1.sh
@@ -8,4 +8,4 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-#sed -i 's/3.05.661/5.04.874/g; s/202b5eb6d4ecee5444ac5a55fea462ad106ebfb88f51ca8499553db4f701f28f/9350a647872c64cc3025b69ae7181de9736b6073acf4b46cad265ee3dad20974/g' ./packages/net/xfrpc/Makefile
+sed -i 's/3.05.661/5.04.874/g; s/202b5eb6d4ecee5444ac5a55fea462ad106ebfb88f51ca8499553db4f701f28f/9350a647872c64cc3025b69ae7181de9736b6073acf4b46cad265ee3dad20974/g' ./packages/net/xfrpc/Makefile
