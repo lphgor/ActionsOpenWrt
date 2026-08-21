8a443f908ee64bd6850b02b0f826660cdff6d648
lphgor <3890346+lphgor@users.noreply.github.com>
2026-08-21 12:07:47 +0800

    Update ax6000.diy-part2.sh

--- ax6000.diy-part2.sh
+++ ax6000.diy-part2.sh
@@ -19,5 +19,5 @@ sed -i '/log-facility/d' ./package/emortal/default-settings/files/99-default-set
 sed -i "/exit 0/ised -i '/DISTRIB_DESCRIPTION/ { s/SNAPSHOT/SNAPSHOT($(date +%Y%m%d%H%M%S))/g; }' /etc/openwrt_release" ./package/emortal/default-settings/files/99-default-settings
 sed -i "/exit 0/ised -i '/OPENWRT_RELEASE/ { s/SNAPSHOT/SNAPSHOT($(date +%Y%m%d%H%M%S))/g; }' /usr/lib/os-release" ./package/emortal/default-settings/files/99-default-settings
 sed -i 's/5.06.909/5.09.976/g; s/7742bedbe929f5bfb386af2025de744aa38bc6f531e1f86a1fbb7318e3ec8f72/C2FDEC14510CF2F25CA086A35EB7657695467EED908A710D302E6B9FC77D1954/g' ./feeds/packages/net/xfrpc/Makefile
-rm -rf rm -rf ./feeds/luci/applications/luci-app-xfrpc
+rm -rf ./feeds/luci/applications/luci-app-xfrpc
 git clone https://github.com/liudf0716/luci-app-xfrpc ./feeds/luci/applications/luci-app-xfrpc
