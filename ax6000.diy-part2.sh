e8c5f5dbcfb26f7883cddaf1308233849055b039
lphgor <3890346+lphgor@users.noreply.github.com>
2026-09-18 18:30:23 +0800

    Update ax6000.diy-part2.sh

--- ax6000.diy-part2.sh
+++ ax6000.diy-part2.sh
@@ -20,5 +20,4 @@ sed -i "/exit 0/ised -i '/DISTRIB_DESCRIPTION/ { s/SNAPSHOT/SNAPSHOT($(date +%Y%
 sed -i "/exit 0/ised -i '/OPENWRT_RELEASE/ { s/SNAPSHOT/SNAPSHOT($(date +%Y%m%d%H%M%S))/g; }' /usr/lib/os-release" ./package/emortal/default-settings/files/99-default-settings
 sed -i 's/5.06.909/5.09.976/g; s/7742bedbe929f5bfb386af2025de744aa38bc6f531e1f86a1fbb7318e3ec8f72/c2fdec14510cf2f25ca086a35eb7657695467eed908a710d302e6b9fc77d1954/g' ./feeds/packages/net/xfrpc/Makefile
 rm -rf ./feeds/luci/applications/luci-app-xfrpc
-git clone https://github.com/liudf0716/luci-app-xfrpc ./feeds/luci/applications/luci-app-xfrpc
-sed -i '/require tools\.github as github/d;/m\.description = github\.desc(/N;s/m\.description = github\.desc(\n[[:space:]]*'\''\(xfrpc is a c language frp client for frps\.\)'\''.*/m.description = _('\''\1'\'');/' ./feeds/luci/applications/luci-app-xfrpc/htdocs/luci-static/resources/view/xfrpc.js
+git clone https://github.com/lphgor/luci-app-xfrpc ./feeds/luci/applications/luci-app-xfrpc
